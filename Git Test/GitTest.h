32abe0b00ad73e934f3ce6deac1ddae6547a6a7b
Andrey <Andrey@Andreys-Mac.local>
2015-02-15 16:57:27 -0500

    added a test

---
+++ Git Test/GitTest.h
@@ -0,0 +1,13 @@
+//
+//  GitTest.h
+//  Git Test
+//
+//  Created by Andrey on 2/15/15.
+//  Copyright (c) 2015 Andrey Kats. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitTest : NSObject
+
+@end
---
+++ Git Test/GitTest.m
@@ -0,0 +1,13 @@
+//
+//  GitTest.m
+//  Git Test
+//
+//  Created by Andrey on 2/15/15.
+//  Copyright (c) 2015 Andrey Kats. All rights reserved.
+//
+
+#import "GitTest.h"
+
+@implementation GitTest
+
+@end
